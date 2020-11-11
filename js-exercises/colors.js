0e8bbe0aeb077b72c68cd29db3b804aca147db39
emtom <emtom22@gmail.com>
2020-11-11 11:30:42 -0800

    change text color based on bckgrnd color

--- js-exercises/colors.js
+++ js-exercises/colors.js
@@ -1,15 +1,38 @@
 const btn = document.querySelector("BUTTON");
 btn.addEventListener("click", generateRGB);
 
-function randomNumber(max, min = 0) {
-  return Math.round(Math.random() * (max - min) + min);
+function generateRGB() {
+  const red = randomNumber(155);
+  const green = randomNumber(155);
+  const blue = randomNumber(155);
+  const rgbColor = `RGB(${red}, ${green}, ${blue})`;
+  document.body.style.backgroundColor = rgbColor;
+  const darkColorThreshold = 50;
+  const colors = [red, green, blue];
+  let isColorDark = true;
+  for (color of colors) {
+    if (color > darkColorThreshold) {
+      isColorDark = false;
+    }
+  }
+  updateText(rgbColor, isColorDark);
 }
 
-function generateRGB() {
-  const red = randomNumber(255);
-  const green = randomNumber(255);
-  const blue = randomNumber(255);
-  const rgbColor = `rgb(${red},${green},${blue})`;
-  document.querySelector("H2").innerHTML = rgbColor;
-  document.body.style.backgroundColor = rgbColor;
+function updateText(color, isColorDark) {
+  const h2 = document.querySelector("H2");
+  const h1 = document.querySelector("H1");
+  h2.innerHTML = color;
+  if (isColorDark) {
+    h1.style.color = "white";
+    h2.style.color = "white";
+  } else {
+    h1.style.color = "black";
+    h2.style.color = "black";
+  }
+
+  i;
+}
+
+function randomNumber(max, min = 0) {
+  return Math.round(Math.random() * (max - min) + min);
 }
