7f1c2610e975e7aa893e0295dc35c5a263b4d0e5
emtom <emtom22@gmail.com>
2020-11-17 23:17:12 -0800

    added js psuedo code

--- js-exercises/tv.shows.js
+++ js-exercises/tv.shows.js
@@ -1,1 +1,9 @@
-tv-shows
+1. On Search, query the API with the given string
+    > API = http://api.tvmaze.com/search/shows?q=girls
+2. get the results, and save the...
+    show -> image -> medium
+    show -> name
+3. add Step 2 to a new img tag with src and alt from vars above
+    > add the class tv-thumbnail
+4. add the img tag to the div
+
