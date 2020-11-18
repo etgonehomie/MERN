40789e3d5e68b02009c7a4e47d0127ffabfbea09
emtom <emtom22@gmail.com>
2020-11-17 23:22:16 -0800

    added js for creating new img

--- js-exercises/tv.shows.js
+++ js-exercises/tv.shows.js
@@ -1,9 +1,15 @@
-1. On Search, query the API with the given string
-    > API = http://api.tvmaze.com/search/shows?q=girls
-2. get the results, and save the...
-    show -> image -> medium
-    show -> name
-3. add Step 2 to a new img tag with src and alt from vars above
-    > add the class tv-thumbnail
-4. add the img tag to the div
+// 1. On Search, query the API with the given string
+//     > API = http://api.tvmaze.com/search/shows?q=girls
+// 2. get the results, and save the...
+//     show -> image -> medium
+//     show -> name
 
+const tvListDiv = document.querySelector("#tv-list");
+
+function searchForTvShows(show) {
+  const tvThumbnail = document.createElement("IMG");
+  tvThumbnail.src = "dfdf";
+  tvThumbnail.alt = "name";
+  tvThumbnail.classList.add("tv-thumbnail");
+  tvListDiv.append(tvThumbnail);
+}
