25fcf808449f699f2d0fa511e0bdbd85e89cb44a
emtom <emtom22@gmail.com>
2020-11-24 23:35:35 -0800

    ENCH: updated server to listen to requests

--- node-exercises/first-server/index.js
+++ node-exercises/first-server/index.js
@@ -1,18 +1,19 @@
 // NPM Imports
-const const express = require('express');
+const express = require("express");
 
-// Setting up the express server
+/// Setting up the express server
 
 // 1. Creates a stagnant server
-const app = express ()
+const app = express();
 
 // 2. Allows the server to receive requests for a specific 'lane' (aka port). Basically allows the server to be utilized
-const portNumber = 3000     // 3000 and 8080 are local hosts
+// CTRL + C to stop the server from listening
+const portNumber = 3000; // 3000 and 8080 are local hosts
 app.listen(portNumber, () => {
-    console.log(`Listening on Port#${portNumber}`)
-})
+  console.log(`Listening on Port#${portNumber}`);
+});
 
 // 3. This function is called for ANY income request to the server. An incoming request is for example, going to a specific webpage, or clicking a button to navigate somewhere.
-app.use() => {
-
-}
+app.use(() => {
+  console.log("Request made to server");
+});
