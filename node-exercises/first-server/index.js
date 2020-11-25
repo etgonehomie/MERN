6f8a7c0e79ba254e0ffc1dd6af16e44fce13b8f4
emtom <emtom22@gmail.com>
2020-11-24 23:24:08 -0800

    ENCH: added boilerplate code for an express app

---
+++ node-exercises/first-server/index.js
@@ -0,0 +1,18 @@
+// NPM Imports
+const const express = require('express');
+
+// Setting up the express server
+
+// 1. Creates a stagnant server
+const app = express ()
+
+// 2. Allows the server to receive requests for a specific 'lane' (aka port). Basically allows the server to be utilized
+const portNumber = 3000     // 3000 and 8080 are local hosts
+app.listen(portNumber, () => {
+    console.log(`Listening on Port#${portNumber}`)
+})
+
+// 3. This function is called for ANY income request to the server. An incoming request is for example, going to a specific webpage, or clicking a button to navigate somewhere.
+app.use() => {
+
+}
