95fa51993940a4e7065d62dd8db855b1f8e3bdf2
emtom <emtom22@gmail.com>
2020-12-01 22:17:28 -0800

    bug: added the require mongoose for schema

--- mongo-exercises/4-mongo-express/models/product.js
+++ mongo-exercises/4-mongo-express/models/product.js
@@ -1,30 +1,30 @@
-const { Schema } = require("mongoose");
+const mongoose = require("mongoose");
 
 const validCategories = [
-    'Electronics',
-    'Creative',
-    'Home Goods',
-    'Decorations',
-    'Books'
-]
+  "Electronics",
+  "Creative",
+  "Home Goods",
+  "Decorations",
+  "Books",
+];
 
-const productSchema = new Schema({
+const productSchema = new mongoose.Schema({
   name: {
-      type: String,
-      required: true,
-      maxlength: 20
+    type: String,
+    required: true,
+    maxlength: 20,
   },
   price: {
-      type: Number,
-      required: true,
-      min: 0
+    type: Number,
+    required: true,
+    min: 0,
   },
   category: {
-      type: String,
-      default: "Uncategorized",
-      enum: validCategories 
-  }
-  tag: [String]
-})
+    type: String,
+    default: "Uncategorized",
+    enum: validCategories,
+  },
+  tag: [String],
+});
 
 const Product = mongoose.model("Product", productSchema);
