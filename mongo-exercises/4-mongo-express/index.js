2ba7304317a42aad058098ee788602c9290dbf4d
emtom <emtom22@gmail.com>
2020-12-06 17:58:44 -0800

    Ench: Added route for detailed item view

--- mongo-exercises/4-mongo-express/index.js
+++ mongo-exercises/4-mongo-express/index.js
@@ -7,9 +7,6 @@ const c = require("./constants");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-app.listen(c.serverPort, () => {
-  console.log(`Listening on server Port#${c.serverPort}`);
-});
 
 // Allow serving of static files for templates to use
 app.use(express.static(path.join(__dirname, "/public")));
@@ -35,8 +32,37 @@ mongoose
 // Add needed models
 const Product = require("./models/ProductModel");
 
-// Testing a Get function
+// Get the index page for all items
 app.get("/", async (req, res) => {
   const products = await Product.find({});
   res.render("home", { products });
+});
+
+// Get unique item details page
+app.get("/items", (req, res) => {
+  const { id } = req.query;
+  console.log(`Retrieved in node: ${id}`);
+  // res.redirect("/error");
+  // res.redirect(`items/${id}`);
+});
+
+app.get("/items/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    const tags = product.tag;
+    res.render("item", { product, tags });
+  } catch (err) {
+    console.log(err);
+    res.render("error");
+  }
+});
+
+// Default any page not available to the error page
+app.use((req, res) => {
+  res.render("error");
+});
+
+app.listen(c.serverPort, () => {
+  console.log(`Listening on server Port#${c.serverPort}`);
 });
--- mongo-exercises/4-mongo-express/public/js/app.js
+++ mongo-exercises/4-mongo-express/public/js/app.js
@@ -1,8 +1,8 @@
-const Product = require("../../models/ProductModel");
-
 // Query Selector for all rows in table
 let productTableRows = document.querySelectorAll("#product-entries tr");
-
+let deleteProductButtons = document.querySelectorAll(
+  "#product-entries .delete-product-btn"
+);
 // Create event listeners for every row
 for (const product of productTableRows) {
   // product.removeEventListener("click", clickHandler, false);
@@ -12,7 +12,9 @@ for (const product of productTableRows) {
 
     // On click of the row navigate to a new detials screen
     const id = product.id;
-    Product.findById(id);
-    // TODO: Navigate to: "http://localhost:3000/items/new" method="GET">
+    window.location.href = `http://localhost:3000/items/${id}`;
   });
+}
+
+for (const deleteBtn of deleteProductButtons) {
 }
